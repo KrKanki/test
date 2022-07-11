9ea97d45b05f187a1cd53de692d5c7d93ae76c38
krkanki <tkdgus403@naver.com>
2022-07-11 12:37:55 +0900

    test

---
+++ src/main/java/com/example/shkim/ShkimApplication.java
@@ -0,0 +1,13 @@
+package com.example.shkim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShkimApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShkimApplication.class, args);
+    }
+
+}
